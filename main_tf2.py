fc83052fecbe37dd4a8c4f7f2f54671dc0d74aba
Quentin Young <qlyoung@qlyoung.net>
2016-10-23 19:38:01 -0400

    more stuff

--- main_tf2.py
+++ main_tf2.py
@@ -2,7 +2,7 @@ import numpy as np
 import csv
 import tflearn
 import tensorflow as tf
-from sklearn.feature_selection import VarianceThreshold
+from sklearn.metrics import accuracy_score
 
 def load_training_data(filename):
   data, labels = [], []
@@ -14,8 +14,6 @@ def load_training_data(filename):
       labels.append(row.pop(-1))
       data.append(np.asarray(row, dtype=np.float32))
     data = np.array(data)
-    sel = VarianceThreshold(threshold=.01)
-    data = sel.fit_transform(data)
     labels = np.array(labels, dtype=np.int)
     labels = tflearn.data_utils.to_categorical(labels, 2)
     return (data, labels)
@@ -37,18 +35,18 @@ t_data, t_labels = load_training_data('numerai_training_data.csv')
 print(t_data)
 print(t_labels)
 
+model_file = open("model.tf", 'w')
+
 # input layer takes an unspecified amount of 21-vectors
 net = tflearn.input_data(shape=[None, 21])
-net = tflearn.fully_connected(net, 32, bias_init='normal')
-net = tflearn.fully_connected(net, 32, bias_init='normal')
-net = tflearn.fully_connected(net, 2, activation='softmax')
+net = tflearn.fully_connected(net, 8)
+net = tflearn.fully_connected(net, 8)
+net = tflearn.fully_connected(net, 2, activation='sigmoid')
 
-sgd = tflearn.SGD(learning_rate=.7, lr_decay=0.96, decay_step=10000)
+sgd = tflearn.SGD(learning_rate=0.1)
 net = tflearn.regression(net, optimizer=sgd, loss='softmax_categorical_crossentropy')
 
 model = tflearn.DNN(net)
-model.fit(t_data, t_labels, n_epoch=500000, batch_size=256, show_metric=True)
-
-with open("model.tf", 'w') as model_file:
-  model.save(model_file)
-  model_file.close()
+model.fit(t_data, t_labels, n_epoch=500000, batch_size=128, show_metric=True)
+model.save(model_file)
+model_file.close()
