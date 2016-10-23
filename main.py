8e4d58bdc3cf99aa61e93953aac6a1d1aa443c96
Quentin Young <qlyoung@qlyoung.net>
2016-10-22 20:18:21 -0400

    ayy lmao

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
+# utilities
 import sys
 import csv
 from sys import argv
-from models import *
+# from models import *
+import numpy as np
+from sklearn.metrics import log_loss
+from sklearn.metrics import accuracy_score
+# classifiers
+from sklearn.svm import SVC
+from sklearn.naive_bayes import GaussianNB
+from sklearn.neural_network import MLPClassifier
+from sklearn.neural_network import MLPRegressor
+from sklearn.linear_model import LogisticRegressionCV
 
 # Configuration parameters
 crossval_percentage = 0.8
@@ -9,10 +19,11 @@ crossval_percentage = 0.8
 
 # Available classifiers to choose from
 classifiers = {
-    'svm': SvmModel,
-    'gaussiannb': GaussianNBModel,
-    'mlp': MLPCModel,
-    'lr': LogisticRegressionModel
+    'svm': SVC(),
+    'gaussiannb': GaussianNB(),
+    'mlp': MLPClassifier(),
+    'mlpr': MLPRegressor(solver='sgd'),
+    'lr': LogisticRegressionCV()
 }
 
 # Data processing
@@ -37,8 +48,22 @@ def process_input_csv(filename):
     with open(filename) as dataset_csv:
         reader = csv.reader(dataset_csv)
         next(reader, None) # skip header
-        dataset = [ map(lambda x: float(x), row) for row in reader ]
-    return predictionset
+        dataset = [ (row[0], list(map(lambda x: float(x), row[1:]))) for row in reader ]
+    return dataset
+
+# fitting function for general model
+def fit(features, labels, classifier):
+    '''
+    Fit the model to a set of data.
+    training_data: [([feature_vec], label)]
+    '''
+    classifier.fit(features, labels)
+
+def predict(features, classifier):
+    '''
+    Generate predictions using the fitted classifier
+    '''
+    return classifier.predict(features)
 
 
 # User interface
@@ -65,16 +90,59 @@ if __name__ == "__main__":
     input_filename = argv[4] if len(argv) > 4 else None
 
     # build training dataset
-    training_data = process_training_csv(training_filename)
+    t_data      = process_training_csv(training_filename)
+    t_features  = np.array([entry[0] for entry in t_data])
+    t_labels    = np.array([entry[1] for entry in t_data])
 
     # get classifier
-    classifier = classifiers[classifier_name]()
+    classifier = classifiers[classifier_name]
 
     if argv[1] == "crossvalidate":
-        score = classifier.crossvalidate(training_data, crossval_percentage)
+        # split the data
+        split_idx = int(crossval_percentage * len(t_data))
+        # split training data into training and validation data
+        cv_t_features = t_features[:split_idx]
+        cv_t_labels   = t_labels[:split_idx]
+        cv_p_features = t_features[split_idx:]
+        cv_p_labels   = t_labels[split_idx:]
+
+        assert len(cv_t_features) == len(cv_t_labels), "Feature length inequal."
+        print("Training set size: {0}".format(len(cv_t_features)))
+        print("Validation set size: {0}".format(len(cv_p_features)))
+
+        # train with the training part
+        print("Fitting...")
+        fit(cv_t_features, cv_t_labels, classifier)
+
+        # make predictions on the other part
+        print("Classifying...")
+        predictions = predict(cv_p_features, classifier)
+
+        # normalize predictions to classes
+        normalized = list(map(lambda x: round(x), predictions))
+        score = accuracy_score(normalized, cv_p_labels)
         print("Accuracy of {0}: {1}".format(classifier_name, score))
+        if hasattr(classifier, 'loss_'):
+            print("Log-loss: {0}".format(classifier.loss_))
 
     elif argv[1] == "classify":
-        prediction_data = process_input_csv(input_filename)
-        classifier.fit(training_data)
-        predictions = classifier.classify(prediction_data)
+        p_data = process_input_csv(input_filename)
+        p_tags = np.array([entry[0] for entry in p_data])
+        print(p_tags)
+        p_features = np.array([entry[1] for entry in p_data])
+
+        print("Training set size: {0}".format(len(t_features)))
+
+        # train
+        print("Fitting...")
+        fit(t_features, t_labels, classifier)
+
+        # predict
+        print("Classifying...")
+        predictions = predict(p_features, classifier)
+
+        print('"t_id","probability"')
+        # dump predictions
+        results = list(zip(p_tags, predictions))
+        for pair in results:
+            print("{0},{1}".format(pair[0], pair[1]))
