1f5cfaed322be33823f997818e59ceb6ba2030d3
Quentin Young <qlyoung@qlyoung.net>
2016-10-24 02:25:18 -0400

    up fp precision to 128

--- main.py
+++ main.py
@@ -9,13 +9,13 @@ def load_training_data(filename):
   with open(filename) as dataset_csv:
     reader = csv.reader(dataset_csv)
     next(reader, None) # skip header
+
     # extract feature vectors and labels
     for row in reader:
       labels.append(row.pop(-1))
-      data.append(np.asarray(row, dtype=np.float32))
+      data.append(np.asarray(row, dtype=np.float128))
     data = np.array(data)
-    sel = VarianceThreshold(threshold=.01)
-    data = sel.fit_transform(data)
+
     labels = np.array(labels, dtype=np.int)
     labels = tflearn.data_utils.to_categorical(labels, 2)
     return (data, labels)
@@ -28,7 +28,7 @@ def load_input_data(filename):
     # extract feature vectors and tags
     for row in reader:
       tags.append(row.pop(0))
-      data.append(np.asarray(row, np.float))
+      data.append(np.asarray(row, np.float128))
     return (data, tags)
 
 # process input data
@@ -37,13 +37,15 @@ t_data, t_labels = load_training_data('numerai_training_data.csv')
 print(t_data)
 print(t_labels)
 
+print("t_data[0][0]: {0}\nt_data[0]: {1}".format(t_data[0][0], t_data[0]))
+
 # input layer takes an unspecified amount of 21-vectors
 net = tflearn.input_data(shape=[None, 21])
-net = tflearn.fully_connected(net, 32, bias_init='normal')
-net = tflearn.fully_connected(net, 32, bias_init='normal')
-net = tflearn.fully_connected(net, 2, activation='softmax')
+net = tflearn.fully_connected(net, 8)
+#net = tflearn.fully_connected(net, 8)
+net = tflearn.fully_connected(net, 2)
 
-sgd = tflearn.SGD(learning_rate=.7, lr_decay=0.96, decay_step=10000)
+sgd = tflearn.SGD(learning_rate=.5, lr_decay=0.96, decay_step=10000)
 net = tflearn.regression(net, optimizer=sgd, loss='softmax_categorical_crossentropy')
 
 model = tflearn.DNN(net)
